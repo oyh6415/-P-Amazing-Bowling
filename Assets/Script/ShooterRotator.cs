c777256734d9deb28136fac7cc8abc8a202b1f44
oyh6415 <dudgus6324@naver.com>
2021-01-15 14:36:34 +0900

    Shooter Rotate2
    포탄 발사대 회전 코드 수정 (if문에서 switch문으로)

--- Assets/Script/ShooterRotator.cs
+++ Assets/Script/ShooterRotator.cs
@@ -14,34 +14,36 @@ public class ShooterRotator : MonoBehaviour
 
     private void Update()
     {
-        if (state == RotateState.Idle)
+        switch (state)
         {
-            if (Input.GetButtonDown("Fire1")) //왼쪽 컨트롤, 왼쪽 마우스
-            {
-                state = RotateState.Horizontal;
-            }
-        }
-        else if (state == RotateState.Horizontal)
-        {
-            if (Input.GetButton("Fire1"))
-            {
-                transform.Rotate(new Vector3(0, horizontalRotateSpeed * Time.deltaTime, 0));
-            }
-            else if (Input.GetButtonUp("Fire1"))
-            {
-                state = RotateState.Vertical;
-            }
-        }
-        else if (state == RotateState.Vertical)
-        {
-            if (Input.GetButton("Fire1"))
-            {
-                transform.Rotate(new Vector3(-verticalRotateSpeed * Time.deltaTime, 0, 0));
-            }
-            else if (Input.GetButtonUp("Fire1"))
-            {
-                state = RotateState.Ready;
-            }
+            case RotateState.Idle:
+                if (Input.GetButtonDown("Fire1")) //왼쪽 컨트롤, 왼쪽 마우스
+                {
+                    state = RotateState.Horizontal;
+                }
+                break;
+            case RotateState.Horizontal:
+                if (Input.GetButton("Fire1"))
+                {
+                    transform.Rotate(new Vector3(0, horizontalRotateSpeed * Time.deltaTime, 0));
+                }
+                else if (Input.GetButtonUp("Fire1"))
+                {
+                    state = RotateState.Vertical;
+                }
+                break;
+            case RotateState.Vertical:
+                if (Input.GetButton("Fire1"))
+                {
+                    transform.Rotate(new Vector3(-verticalRotateSpeed * Time.deltaTime, 0, 0));
+                }
+                else if (Input.GetButtonUp("Fire1"))
+                {
+                    state = RotateState.Ready;
+                }
+                break;
+            case RotateState.Ready:
+            break;
         }
     }
 }

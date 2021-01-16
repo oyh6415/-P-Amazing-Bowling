81a540715fa76231055009f258daed6a0b1ab367
oyh6415 <dudgus6324@naver.com>
2021-01-16 23:40:54 +0900

    Camera Tracking
    * 포탄을 따라 움직이는 카메라 구현
    * 포탄 발사 여부에 따른 줌 인, 아웃 구현

---
+++ Assets/Scripts/CamFollow.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamFollow : MonoBehaviour
+{
+    public enum State
+    {
+        Idle,Ready,Tracking
+    }
+    
+    private State state
+    {
+        set
+        {
+            switch (value)
+            {
+                case State.Idle:
+                    targetZoomSize = roundReadZoomSize;
+                    break;
+                case State.Ready:
+                    targetZoomSize = readyShotZoomSize;
+                    break;
+                case State.Tracking:
+                    targetZoomSize = trackingZoomSize;
+                    break;
+            }
+        }
+    }
+
+    private Transform target;
+
+    public float smoothTime = 0.2f; //지연 시간
+    private Vector3 lastMovingVelocity;
+    private Vector3 movingVelocity;
+    private Vector3 targetPosition;
+
+    private Camera cam;
+    private float targetZoomSize = 5f;
+
+    private const float roundReadZoomSize = 14.5f;
+    private const float readyShotZoomSize = 5f;
+    private const float trackingZoomSize = 10f;
+
+    private float lastZoomSpeed;
+
+    private void Awake()
+    {
+        cam = GetComponentInChildren<Camera>();
+        state = State.Idle;
+    }
+
+    private void Move()
+    {
+        targetPosition = target.transform.position;
+        Vector3 smoothPostion = Vector3.SmoothDamp(transform.position, targetPosition,
+            ref lastMovingVelocity, smoothTime);
+
+        transform.position = smoothPostion;
+    }
+
+    private void Zoom()
+    {
+        float smoothZoomSize = Mathf.SmoothDamp(cam.orthographicSize, targetZoomSize,
+            ref lastZoomSpeed, smoothTime);
+
+        cam.orthographicSize = smoothZoomSize;
+    }
+
+    private void FixedUpdate()
+    {
+        if (target != null)
+        {
+            Move();
+            Zoom();
+        }
+    }
+
+    public void ResetState()
+    {
+        state = State.Idle;
+    }
+
+    public void SetTarget(Transform newTarget, State newState)
+    {
+        target = newTarget;
+        state = newState;
+    }
+}
